b6448b91a6f4fc23033bc7786d17b0bd933564f9
HaeJoon <wjdgowns4414@gmail.com>
2025-03-07 10:36:35 +0900

    tree 조회 수정

--- src/main/java/com/example/tree/tree/TreeService.java
+++ src/main/java/com/example/tree/tree/TreeService.java
@@ -2,17 +2,21 @@ package com.example.tree.tree;
 
 import com.example.tree.User.User;
 import com.example.tree.User.UserRepository;
+import com.example.tree.letter.dto.LetterSimpleResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 public class TreeService {
     private final TreeRepository treeRepository;
     private final UserRepository userRepository;
+    private final TreeDao treeDao;
 
-    public TreeService(TreeRepository treeRepository, UserRepository userRepository, UserRepository userRepository1) {
+    public TreeService(TreeDao treeDao, TreeRepository treeRepository, UserRepository userRepository, UserRepository userRepository1) {
+        this.treeDao = treeDao;
         this.treeRepository = treeRepository;
         this.userRepository = userRepository1;
     }
@@ -22,8 +26,11 @@ public class TreeService {
     }
 
     public TreeResponse read(Long treeId) {
-        Tree tree = treeRepository.findById(treeId).orElseThrow(() -> new NoSuchElementException("id가 없습니다."));
-        return new TreeResponse(tree.getId(),tree.getTitle(),tree.getUrlPath(),tree.isOpen(),tree.getUser().getId(),tree.getLetterList());
+        Tree tree = treeDao.findByTreeIdAndIsOpen(treeId);
+        List<LetterSimpleResponse> letterSRPList = tree.getLetterList().stream()
+                .map(LetterSimpleResponse::toDto)
+                .toList();
+        return new TreeResponse(tree.getId(),tree.getTitle(),tree.getUrlPath(),tree.isOpen(),tree.getUser().getId(),letterSRPList);
     }
     @Transactional
     public void delete(Long treeId) {
